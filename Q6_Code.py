fe9d2dbf6039eb29c58e70f9f6ed23245408712c
fheavr <gilletrj@gmail.com>
2013-09-17 17:25:51 -0700

    allow for heuristic selection of nodes
    implement a means of using a heuristic to decide on the next node to be
    chosen while building tree

--- Q6_Code.py
+++ Q6_Code.py
@@ -6,15 +6,22 @@
 import string
 
 class Node:
-    def __init__(self, name, domain):
+    def __init__(self, name, domain, heuristic):
         self.domain = list(domain)
         # iterate over the domain to find solution
         self.value  = 0
         self.rules  = []
         self.active = False
-        self.next_node = None
+        self.heuristic = heuristic
         self.last = None
         self.name = name
+
+        # to be populated, a node weighting for node selection
+        # live_weight is the number of rules that will restrict the new node
+        # dead_weight is the number of rules that this node features in that
+        #    will not be active after this node becomes active
+        self.live_weight = 0
+        self.dead_weight = 0
 
     def __repr__(self):
         return "{0}={1} ".format(self.name, self.value)
@@ -45,11 +52,11 @@ class Node:
                 leaf_count += 1
                 print "{0} {1} failure".format(state, self)
                 continue
-            elif self.next_node == None:
+            elif self.heuristic.h_node() == None:
                 leaf_count += 1
                 print "{0} {1} solution".format(state, self)
             else:
-                leaf_count += self.next_node.iterate("{0} {1}".format(state, self))
+                leaf_count += self.heuristic.h_node().iterate("{0} {1}".format(state, self))
         self.active = 0
         return leaf_count
 
@@ -99,26 +106,71 @@ def abs_diff_odd(node1, node2):
 def abs_diff_eq_1(node1, node2):
     return abs(node2.value - node1.value) == 1
 
+# determine which node to next expand based on the number of
+# constraints in which it features
+class Heuristic:
+
+    def __init__(self, node_list, rule_list):
+        self.node_list = node_list
+        self.rule_list = rule_list
+
+    def h_node(self):
+        for rule in self.rule_list:
+            if rule.node1.active != rule.node2.active:
+                rule.node1.live_weight +=1
+                rule.node2.live_weight +=1
+            elif rule.node1.active == 0:
+                rule.node1.dead_weight +=1
+                rule.node2.dead_weight +=1
+
+        best_live = None
+        best_dead = None
+        for node in self.node_list:
+            if node.active != 1 and \
+               (best_live == None or \
+                node.live_weight > best_live.live_weight):
+                best_live = node
+
+            if node.active != 1 and \
+               (best_dead == None or \
+                node.dead_weight > best_dead.dead_weight):
+                best_dead = node
+
+        # reset weights
+        for node in self.node_list:
+            node.live_weight = 0
+            node.dead_weight = 0
+
+        if best_live != None:
+            return best_live
+        else:
+            return best_dead
+
 def Solve():
+# more efficient selection heuristic
+    
     domain   = [1, 2, 3, 4]
+    nodes    = []
+    rules    = []
+    heuristic = Heuristic(nodes, rules)
 
     #define nodes
-    A = Node('A', domain)
-    B = Node('B', domain)
-    C = Node('C', domain)
-    D = Node('D', domain)
-    E = Node('E', domain)
-    F = Node('F', domain)
+    A = Node('A', domain, heuristic)
+    B = Node('B', domain, heuristic)
+    C = Node('C', domain, heuristic)
+    D = Node('D', domain, heuristic)
+    E = Node('E', domain, heuristic)
+    F = Node('F', domain, heuristic)
 
-    # link nodes
-    A.push(B)
-    B.push(C)
-    C.push(D)
-    D.push(E)
-    E.push(F)
+    # build a list of nodes
+    nodes.append(A)
+    nodes.append(B)
+    nodes.append(C)
+    nodes.append(D)
+    nodes.append(E)
+    nodes.append(F)
 
     #define rules
-    rules = []
     rules.append(Rule(A, B, lt))
     rules.append(Rule(A, C, abs_diff_eq_1))
     rules.append(Rule(B, C, abs_diff_even))
@@ -134,7 +186,7 @@ def Solve():
     rules.append(Rule(D, F, ne))
     rules.append(Rule(E, F, abs_diff_odd))
 
-    num = A.iterate("")
+    num = heuristic.h_node().iterate("")
 
     print "This node selection results in {} leaf nodes".format(num)
 
